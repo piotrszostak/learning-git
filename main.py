158d0f81319d54da063125031b7c6ab676e2691d
Piotr Szostak <pio.szo@proton.me>
2023-10-24 18:56:59 +0200

    Rename variable in for loop

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 message = "'Hiszpańska inkwizycja' to najlepszy skecz grupy Monty Pythona!"
 print(message)
 
-for word in message:
-    print(word)
+for letter in message:
+    print(letter)
 
 # zmiany z gałęzi 'nazwa'
 # zmiany z gałęzi 'inny'
